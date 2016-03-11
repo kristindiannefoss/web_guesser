48e2e1ebd889605d59577e5a166eb5e30924dc62
Kris Foss <kris.foss@gmail.com>
2016-03-11 11:19:54 -0700

    colors and guessing features working

--- web_guesser.rb
+++ web_guesser.rb
@@ -12,17 +12,22 @@ end
       if guess > SECRET_NUMBER
         if guess > SECRET_NUMBER + 5
           @@message = "way too high"
+          @@color   = "red"
         else
           @@message = "too high"
+          @@color   = "salmon"
         end
       elsif guess < SECRET_NUMBER
         if guess < SECRET_NUMBER - 5
           @@message = "way too low"
+          @@color   = "red"
         else
           @@message = "too low"
+          @@color   = "salmon"
         end
       elsif guess == SECRET_NUMBER
-        @@message = "You got it right! The secret number is #{SECRET_NUMBER}"
+        @@message   = "You got it right! The secret number is #{SECRET_NUMBER}"
+        @@color     = "green"
       end
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => @@message}
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => @@message, :color => @@color}
   end
